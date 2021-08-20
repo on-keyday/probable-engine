8b37c1a87db0a787e6375e0135fea612bd84b639
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-08-20 19:04:43 +0900

    add define for linux(incompleted)

--- src/sockbase.h
+++ src/sockbase.h
@@ -2,9 +2,31 @@
 #pragma once
 
 #ifdef _WIN32
+#ifdef __MINGW32__
+#define _WIN32_WINNT 0x0501
+#endif
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #else
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#define closesocket close
+#define ioctlsocket ioctl
+#define SD_SEND SHUT_WR
+#define SD_RECEIVE SHUT_RD
+#endif
+
+#include <string>
+#include <string.h>
+
+#if !defined(_WIN32)||(defined(__GNUC__)&&!defined(__clang__))
+#define memcpy_s(dst, dsz, src, ssz) memcpy(dst, src, ssz)
+#define strcpy_s(dst, dsz, src) strcpy(dst, src)
 #endif
 
 #ifndef USE_OPENSSL
@@ -14,8 +36,6 @@
 #if USE_OPENSSL
 #include <openssl/ssl.h>
 #endif
-
-#include <string>
 
 namespace socklib {
     constexpr int invalid_socket = -1;
