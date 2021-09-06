4595b914e61bb0def9846415b4ab36a8b6b8f5a4
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-09-06 22:51:49 +0900

    fix status code

--- src/http1.h
+++ src/http1.h
@@ -18,10 +18,12 @@ namespace socklib {
                 return "Early Hints";
             case 200:
                 return "OK";
-            case 202:
+            case 201:
                 return "Created";
-            case 203:
+            case 202:
                 return "Accepted";
+            case 203:
+                return "Non-Authoritative Information";
             case 204:
                 return "No Content";
             case 205:
