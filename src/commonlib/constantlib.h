ac7a627482023d8a87fea6407792cb638bb7d2d2
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-09-17 00:07:20 +0900

    update commonlib

--- src/commonlib/constantlib.h
+++ src/commonlib/constantlib.h
@@ -87,4 +87,112 @@ namespace PROJECT_NAME {
         }
     };
 
+    
+    template <class C, size_t size_ = 0>
+    struct ConstString {
+        C buf[size_] = {0};
+
+        constexpr static size_t strsize = size_ - 1;
+        constexpr ConstString() {}
+
+        constexpr ConstString(const ConstString& in) {
+            for (size_t i = 0; i < strsize; i++) {
+                buf[i] = in.buf[i];
+            }
+        }
+
+        constexpr ConstString(const C (&ptr)[size_]) {
+            for (size_t i = 0; i < strsize; i++) {
+                buf[i] = ptr[i];
+            }
+        }
+
+        constexpr size_t size() const {
+            return strsize;
+        }
+
+        constexpr ConstString<C, size_ + 1> push_back(C c) const {
+            C copy[size_ + 1];
+            for (size_t i = 0; i < strsize; i++) {
+                copy[i] = buf[i];
+            }
+            copy[strsize] = c;
+            return ConstString<C, size_ + 1>(copy);
+        }
+
+    private:
+        template <size_t sz, size_t add, size_t srcsize, bool flag = (add == srcsize - 1)>
+        struct Appender {
+            constexpr static auto appending(const ConstString<C, sz + add>& dst, const ConstString<C, srcsize>& src, size_t idx) {
+                auto s = dst.push_back(src[idx]);
+                return Appender<sz, add + 1, srcsize>::appending(s, src, idx + 1);
+            }
+        };
+
+        template <size_t sz, size_t add, size_t srcsize>
+        struct Appender<sz, add, srcsize, true> {
+            constexpr static auto appending(const ConstString<C, sz + add>& dst, const ConstString<C, srcsize>& src, size_t idx) {
+                return ConstString<C, sz + add>(dst);
+            }
+        };
+
+    public:
+        template <size_t add>
+        constexpr auto append(const ConstString<C, add>& src) const {
+            return Appender<size_, 0, add>::appending(*this, src, 0);
+        }
+
+        template <size_t add>
+        constexpr auto append(const C (&str)[add]) const {
+            ConstString<C, add> src(str);
+            return Appender<size_, 0, add>::appending(*this, src, 0);
+        }
+
+        constexpr const C& operator[](size_t idx) const {
+            if (idx >= size_) {
+                throw "out of range";
+            }
+            return buf[idx];
+        }
+
+        constexpr C& operator[](size_t idx) {
+            if (idx >= size_) {
+                throw "out of range";
+            }
+            return buf[idx];
+        }
+
+        constexpr const C* c_str() const {
+            return buf;
+        }
+
+        constexpr C* begin() {
+            return buf;
+        }
+
+        constexpr const C* begin() const {
+            return buf;
+        }
+
+        constexpr C* end() {
+            return buf + strsize;
+        }
+
+        constexpr const C* end() const {
+            return buf + strsize;
+        }
+    };
+
+    template <class C, size_t sz1, size_t sz2>
+    constexpr bool operator==(const ConstString<C, sz1>& s1, const ConstString<C, sz2>& s2) {
+        if (sz1 != sz2) return false;
+        for (size_t i = 0; i < sz1; i++) {
+            if (s1[i] != s2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }  // namespace PROJECT_NAME
