f012928abb1c5f5f1132687a12f2876ff4264fec
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-09-30 12:08:39 +0900

    add refactoring

--- src/v2/tcp.h
+++ src/v2/tcp.h
@@ -2,6 +2,7 @@
 
 #include "iconn.h"
 #include <reader.h>
+#include <callback_invoker.h>
 #include <memory>
 
 namespace socklib {
@@ -11,6 +12,7 @@ namespace socklib {
             resolve_address,
             register_cacert,
             connect,
+            no_address_to_connect,
             canceled,
         };
 
@@ -20,15 +22,16 @@ namespace socklib {
             string_t host;
             string_t service;
             std::uint16_t port = 0;
-            bool secure = false;
+            ConnStat stat;  //to set stat
             string_t cacert;
             int ip_version = 0;
             TCPError err;
         };
 
-        template <class Conn, class String>
-        struct TCP {
-            static bool resolve_detail(const char* host, const char* service, int ipver, ::addringo*& info){} {
+        template <class String>
+        struct Resolver {
+           private:
+            static bool resolve_detail(const char* host, const char* service, int ipver, ::addrinfo*& info) {
                 ::addrinfo hint = {0};
                 hint.ai_socktype = SOCK_STREAM;
                 switch (ipver) {
@@ -42,16 +45,21 @@ namespace socklib {
                         hint.ai_family = AF_UNSPEC;
                         break;
                 }
-                return ::getaddrinfo(ctx.host, ctx.service, &hint, &info) != 0;
+                if (::getaddrinfo(ctx.host, ctx.service, &hint, &info) != 0) {
+                    ctx.err = TCPError::resolve_address;
+                    return false;
+                }
+                return true;
             }
 
+           public:
             template <class C>
-            static bool resolve(TCPOpenContext<C*>& ctx, ::addringo*& info) {
+            static bool resolve(TCPOpenContext<C*>& ctx, ::addrinfo*& info) {
                 return resolve_detail(ctx.host, ctx.service, ctx.ip_version, info);
             }
 
             template <class Str>
-            static bool resolve(TCPOpenContext<Str>& ctx) {
+            static bool resolve(TCPOpenContext<Str>& ctx, ::addrinfo*& info) {
                 return resolve_detail(ctx.host.c_str(), ctx.service.c_str(), ctx.ip_version, info);
             }
 
@@ -59,6 +67,7 @@ namespace socklib {
                 OsErrorContext canceler(cancel);
                 auto tmp = ::socket(p->ai_family, p->ai_socktype, p->ai_protocol);
                 if (tmp < 0) continue;
+                u_long flag = 1;
                 ::ioctlsocket(tmp, FIONBIO, &flag);
                 auto res = ::connect(tmp, info->ai_addr, info->ai_addrlen);
                 if (res == 0) {
@@ -101,25 +110,37 @@ namespace socklib {
                 }
             }
 
-            static bool connect(std::shared_ptr<Conn>& ret, TCPOpenContext<String>& ctx, CancelContext* cancel, ::addrinfo*& info) {
+            template <class Check = bool (*)(::addrinfo*, bool*)>
+            static bool connect(TCPOpenContext<String>& ctx, CancelContext* cancel, int& sock, ::addrinfo* info, ::addrinfo*& selected, Check&& check = Check()) {
                 std::uint16_t port = commonlib2::translate_byte_net_and_host<std::uint16_t>(&ctx.port);
                 for (auto p = info; p; p = p->ai_next) {
-                    sockaddr_in* addr = (sockaddr_in*)p->ai_addr;
+                    ::sockaddr_in* addr = (::sockaddr_in*)p->ai_addr;
                     if (port) {
                         addr->sin_port = port;
                     }
-                    int sock = 0;
+                    bool result = false;
+                    if (!commonlib2::invoke_cb<Check, bool>::invoke(std::forward<Check>(check), p, &result)) {
+                        return result;
+                    }
                     if (connect_loop(p, sock, ctx, cancel)) {
+                        selected = p;
+                        return true;
                     }
                 }
+                ctx.err = TCPError::no_address_to_connect;
+                return false;
             }
+        };
 
+        template <class String>
+        struct TCP {
             static std::shared_ptr<InetConn> open(TCPOpenContext<String>& ctx, CancelContext* cancel = nullptr) {
-                std::shared_ptr<Conn> ret;
+                if (ctx.stat.type != ConnType::tcp_over_ssl) {
+                    ctx.stat.type = ConnType::tcp_socket;
+                }
                 ::addrinfo* info;
-                if (!resolve(ctx, info)) {
-                    ctx.err = TCPError::resolve_address;
-                    return nullptr;
+                if (!Resolver<String>::resolve(ctx, info)) {
+                    return false;
                 }
             }
         };
