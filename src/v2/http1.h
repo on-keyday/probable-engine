c89b30fd378f66440bb81a1ff1e862ab2a4ba96d
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-09-30 13:09:50 +0900

    add refactoring

--- src/v2/http1.h
+++ src/v2/http1.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "iconn.h"
+#include "tcp.h"
+
+#include <net_helper.h>
 
 namespace socklib {
     namespace v2 {
@@ -14,22 +17,77 @@ namespace socklib {
             closed,
         };
 
+        enum class HttpError {
+            tcp_error,
+            url_parse,
+            url_encode,
+        };
+
         template <class String, class Header, class Body>
         struct RequestContext {
             using string_t = String;
             using header_t = Header;
             using body_t = Body;
             RequestPhase phase;
+            string_t method;
             string_t url;
+            string_t default_path;
+            string_t default_scheme;
+            bool urlencoded = false;
+            bool use_proxy = false;
             string_t proxy;
             header_t request;
             body_t requestbody;
             header_t response;
             body_t responsebody;
+            TCPError tcperr;
+            HttpError err;
         };
 
-        struct HttpConn : private SecureStreamConn {
-            using SecureStreamConn::SecureStreamConn;
+        template <class String, class Header, class Body>
+        struct URLParser {
+            using string_t = String;
+            using request_t = RequestContext<String, Header, Body>;
+            using parsed_t = commonlib2::URLContext<String>;
+            bool parse(parsed_t& url, request_t& req) {
+                commonlib2::Reader(req.url).readwhile(commonlib2::parse_url, url);
+                if (!ctx.url.succeed) {
+                    req.err = HttpError::url_parse;
+                    return false;
+                }
+                if (url.scheme.size() == 0) {
+                    url.scheme = req.default_scheme;
+                }
+                if (url.path.size() == 0) {
+                    url.path = req.default_path;
+                }
+                if (req.urlencoded) {
+                    commonlib2::URLEncodingContext<std::string> encctx;
+                    string_t path, query;
+                    encctx.path = true;
+                    commonlib2::Reader(url.path).readwhile(path, commonlib2::url_encode, &encctx);
+                    if (encctx.failed) {
+                        req.err = HttpError::url_encode;
+                    }
+                    encctx.query = true;
+                    encctx.path = false;
+                    commonlib2::Reader(url.query).readwhile(query, commonlib2::url_encode, &encctx);
+                    if (encctx.failed) {
+                        req.err = HttpError::url_encode;
+                    }
+                    url.path = std::move(path);
+                    url.query = std::move(query);
+                }
+                return true;
+            }
+        };
+
+        template <class String, class Header, class Body>
+        struct HttpConn {
+            using request_t = RequestContext<String, Header, Body>;
+            std::shared_ptr<InetConn> conn;
+            bool request(request_t& req) {
+            }
         };
 
     }  // namespace v2
